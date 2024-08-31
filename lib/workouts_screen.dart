ac4972b2fd5b2a6db3f583cf90bfcf512d5860b2
Assassin33GT <bodeidris33@gmail.com>
2024-08-31 18:32:14 +0300

    seconds

--- lib/workouts_screen.dart
+++ lib/workouts_screen.dart
@@ -1,6 +1,5 @@
 import 'package:fitness_tracker/goal_con.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class WorkoutsScreen extends StatelessWidget {
